c1827b5b2a48ca68a3af017f7bf5aaebb11905c9
DaniilRai13 <testnet02@mail.ru>
2023-05-07 16:22:28 +0300

    feat: create function to write in file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,5 +1,29 @@
+let fs = require('fs')
 let rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 })
+
+process.on("exit", () => {
+    console.log("Всего хорошего, до свидания!")
+    writeStream.end('')
+    rl.close()
+})
+
+let writeStream = fs.createWriteStream('./02-write-file/result.txt')
+let writeTextInFile = (greetingText) => {
+    rl.question(greetingText, inputText => {
+        if (inputText === "exit") {
+            writeStream.end('')
+            process.exit()
+        } else {
+            writeStream.write(inputText + '\n')
+            writeTextInFile('Напишите еще что-нибудь. Для выхода нажмите "CTRL+C" или введите "exit". \n')
+        }
+    })
+}
+
+writeTextInFile(`Введите текст, который будет записан в файл:\n`)
+
+
 
